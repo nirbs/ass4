3ba4a0d537a04da73d60da7ab2e15a388cd6e1a8
Matan <matanbn22@gmail.com>
2016-04-20 14:06:01 +0300

    PlusMinus

--- src/Div.java
+++ src/Div.java
@@ -2,15 +2,5 @@
  * Created by user on 13/04/2016.
  */
 public class Div {
-    Var var;
-    Num num;
 
-    public Div (Var var, Num num){
-        this.var=var;
-        this.num=num;
-    }
-
-    public double getDiv(){
-        return var.getValue()/ num.getNum();
-    }
 }
--- src/Log.java
+++ src/Log.java
@@ -2,15 +2,4 @@
  * Created by user on 13/04/2016.
  */
 public class Log {
-    Var var;
-    Num num;
-
-    public Log (Var var, Num num){
-        this.var=var;
-        this.num=num;
-    }
-
-    public double getLog(){
-        return var.getValue()+ num.getNum();
-    }
 }
