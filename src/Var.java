258f343b26ecde1fc9769c62179c7da3205aa62f
nirbs <nirchook@gmail.com>
2016-04-20 12:42:52 +0300

    updated by nir

--- src/Var.java
+++ src/Var.java
@@ -1,27 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by user on 13/04/2016.
  */
 public class Var {
     String var;
-    Expression value;
+    double value;
 
     public Var(String var){
         this.var=var;
-    }
-
-    public String getVar() {
-        return var;
     }
 
     public void setVar(String var) {
         this.var = var;
     }
 
-    public Expression getValue() {
-        return value;
+    public void setValue(double val) {
+        value = val;
     }
 
-    public void setValue(Expression e) {
-        value = e;
+    public double evaluate(Map<String, Double> assignment) throws Exception{
+        return assignment.get(this.var);
     }
+    public double evaluate() throws Exception{
+        return this.value;
+    }
+    public List<String> getVariables(){
+        List l = new ArrayList<String>();
+        l.add(this.var);
+        return l;
+    }
+    public String toString(){
+        return this.var;
+    }
+    public Expression assign(String var, Expression expression){
+        return expression;
+    }
+
 }
