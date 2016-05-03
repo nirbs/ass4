fd303a1beb65b15c89cb9ef65a4fc8a9d3aada55
Matan <matanbn22@gmail.com>
2016-05-03 11:56:16 +0300

    Nana banana

--- src/SimplificationDemo.java
+++ src/SimplificationDemo.java
@@ -1,10 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Created by Matan on 5/1/2016.
+ * The SimplificationDemo class is in charge of showing the advanced simplification.
+ * @author Matan Ben Noach Nir Ben Shalom.
+ * @version 1.0 1 May 2016.
  */
 public class SimplificationDemo {
+    /**
+     * main method demonstrates our advanced simplification.
+     * @param args input from command line.
+     */
     public static void main(String[] args) {
         Expression e = new Pow(new Mult(new Plus("x", "y"), 3), 0);
         System.out.println(e);
@@ -12,6 +16,5 @@ public class SimplificationDemo {
         Expression ex = new Pow(new Pow("x",new Plus(5,3)),"z");
         System.out.println(ex);
         System.out.println(ex.simplify());
-        Expression multPlus = new Plus(new Mult(2,"x"),new Mult(4,"x"));
     }
 }
